5d033c89d5512788ed6f980e5f3745eb2215bfdb
peterlubega <*peterlubega100@gmail.com>
2024-01-27 11:35:57 -0800

    feat: the bitonic file

---
+++ 106-bitonic_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+  * biotonic_sort - A function that sorts an array using biotonic algorithm
+  * @array: The array to sort.
+  * @size: The length of the array.
+  * Return: Nothing.
+  */
+void bitonic_sort(int *array __attribute__((unused)), size_t size __attribute__((unused)))
+{
+/*Function implementation goes here*/
+}
